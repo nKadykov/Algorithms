56e7c02d32d95b84b50d304fc48ad33f4b8fbbf0
nKadykov <nkadykov@gmail.com>
2024-04-10 14:12:00 +0300

    init

---
+++ BubbleSort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+template<typename Item>
+void exch(Item &A, Item &B) {
+    Item t = A;
+    A = B;
+    B = t;
+}
+
+template<typename Item>
+void compexch(Item &A, Item &B) {
+    if(B < A) {
+        exch(A, B);
+    }
+}
+
+template<typename Item>
+void bubble(Item a[], int l, int r) {
+    for(int i = l; i < r; i++) {
+        for(int j = r; j > i; j--) {
+            compexch(a[j - 1], a[j]);
+        }
+    }
+}
+
+int main() {
+    int i, N, sw;
+    std::cout << "N: ";
+    std::cin >> N;
+    std::cout << "sw: ";
+    std::cin >> sw;
+    int *a = new int[N];
+    if(sw) {
+        for(i = 0; i < N; i++) {
+            a[i] = 1000 * (1.0 * rand() / RAND_MAX);
+        }
+    }
+    else {
+        N = 0;
+        while(std::cin >> a[N]) {
+            N++;
+        }
+    }
+    bubble(a, 0, N - 1);
+    for(i = 0; i < N; i++) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << std::endl;
+}
